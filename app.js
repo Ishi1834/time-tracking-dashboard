60a85e0813f54b46a0dc5c76741db57e30fcc012
Ismail Mohamad <ikmohamad19@gmail.com>
2022-01-06 17:04:30 +0000

    Built the javascript interface, will work on the desktop site css

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
+import * as data from "./data.json" assert { type: "json" };
+let json = data.default;
 const daily = document.getElementById("daily");
 const weekly = document.getElementById("weekly");
 const monthly = document.getElementById("monthly");
+weekly.style.color = "white";
 
 daily.addEventListener("click", function () {
-  console.log("swith to daily");
+  //console.log("swith to daily");
   this.style.color = "white";
   weekly.style.color = "";
   monthly.style.color = "";
+  switchTime("daily");
 });
 weekly.addEventListener("click", function () {
-  console.log("swith to weekly");
+  //console.log("swith to weekly");
   this.style.color = "white";
   daily.style.color = "";
   monthly.style.color = "";
+  switchTime("weekly");
 });
 monthly.addEventListener("click", function () {
-  console.log("swith to monthly");
+  //console.log("swith to monthly");
   this.style.color = "white";
   weekly.style.color = "";
   daily.style.color = "";
+  switchTime("monthly");
 });
+function switchTime(value) {
+  var x = document.getElementsByClassName("thisWeek");
+  var y = document.getElementsByClassName("lastWeek");
+  for (let i = 0; i < x.length; i++) {
+    //console.log(x[i]);
+    //console.log(json[i].timeframes[value]);
+    var current = json[i].timeframes[value].current;
+    var previous = json[i].timeframes[value].previous;
+    x[i].innerHTML = current + "hrs";
+    y[i].innerHTML = "Last Week - " + previous + "hrs";
+  }
+}
